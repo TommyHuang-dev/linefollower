e1c039a8d82a40620a561e62360e643b0513fc3a
Tommy Huang <tommyhuang999@gmail.com>
2018-06-19 13:19:44 -0400

    finetweak some values

--- linefollower/linefollower.ino
+++ linefollower/linefollower.ino
@@ -3,7 +3,8 @@
 #define LSensorPort A5
 #define MSensorPort A4
 #define RSensorPort A3
-#define boundary 3.6
+#define boundary 3.2
+#define motorSpeed 150
 
 
 void setup() {
@@ -19,33 +20,33 @@ void setup() {
   
 }
 
-float sensorValues[3];
+
 void loop() {
   // put your main code here, to run repeatedly:
   // get the voltage of all sensors
   int sensorL = analogRead(LSensorPort);
   int sensorM = analogRead(MSensorPort);
   int sensorR = analogRead(RSensorPort);
-  float voltageL = (float)sensorL * 5.0 / 1023.0;
-  float voltageM = (float)sensorM * 5.0 / 1023.0;
-  float voltageR = (float)sensorR * 5.0 / 1023.0;
+  double voltageL = (double)sensorL * 5.0 / 1023.0;
+  double voltageM = (double)sensorM * 5.0 / 1023.0;
+  double voltageR = (double)sensorR * 5.0 / 1023.0;
   
   // keep going straight if the middle sensor is high
-  if (voltageM >= boundary && voltageL < boundary && voltageR < boundary) {
+  if (voltageM >= boundary - 0.5 && voltageL < boundary && voltageR < boundary) {
     // go forward
-    analogWrite(LMotorPort, 128);
-    analogWrite(RMotorPort, 128);
+    analogWrite(LMotorPort, motorSpeed);
+    analogWrite(RMotorPort, motorSpeed);
   }
   // power right wheel to go left if only left sensor is high
   else if (voltageL >= boundary && voltageR < boundary) {
     // turn left
     analogWrite(LMotorPort, 0);
-    analogWrite(RMotorPort, 128);
+    analogWrite(RMotorPort, motorSpeed);
   }
   // power left wheel to go right if only right sensor is high
   else if (voltageL < boundary && voltageR >= boundary) {
     // turn right
-    analogWrite(LMotorPort, 128);
+    analogWrite(LMotorPort, motorSpeed);
     analogWrite(RMotorPort, 0);
   }
   // if all three sensors are high, stop.
@@ -54,16 +55,15 @@ void loop() {
     digitalWrite(LMotorPort, LOW);
     digitalWrite(RMotorPort, LOW);
   }
-  /*
-  Serial.print("L: ");
-  Serial.println(voltageL);
   
-  Serial.print("M: ");
+//  Serial.print("L: ");
+//  Serial.println(voltageL);
+  
+//  Serial.print("M: ");
   Serial.println(voltageM);
 
-  Serial.print("R: ");
-  Serial.println(voltageR);
-  delay(100); */
+//  Serial.print("R: ");
+//  Serial.println(voltageR);
   // only for the serial monitor, delete or comment out if not needed
 }
 
